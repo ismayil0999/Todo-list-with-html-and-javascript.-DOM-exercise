381e67586337b2a68abea580ae76a9b47dc65c10
hebibov666 <asuslap503@gmail.com>
2024-05-06 20:35:55 +0300

    Changed structure of todo element, delete RemoveAllTodo function

--- script.js
+++ script.js
@@ -2,31 +2,26 @@
 let input=document.getElementById("input");
 let output=document.getElementById("output");
 let btn=document.getElementById("add-btn");
-let info=document.getElementById("todos-info");
 let modal=document.getElementById("modal-container");
 let searchInput=document.getElementById("search-input");
 //Todo əlavə edilməsi üçün funksiya
 
 function openTodoInput(){
-modal.style.display="flex"
+modal.style.display="flex";
+input.focus()
 }
 function addTodo(){
     let todo = input.value;
     //Yeni li elementi yaradır
     let li = document.createElement("li");
-    li.innerHTML = `<span class="todo-content-box">
-    <i class="fa-solid fa-sort"></i>
+    li.innerHTML = `
     <input value="${todo}" readonly="readonly"></input>
-    </span>
-    <span> 
     <i class="fa-solid fa-pen" onClick="editTodo(this)"></i>
     <i  class="fa-solid fa-trash" onClick="deleteTodo(this)"></i>
-    </span>`;
+    `;
 
     if (todo !== "") {
         let firstTodo = output.firstChild;
-        //Todo-ların sayını almaq üçün.Dokument içərisində nə qədər li elementi varsa hamısını seçirik və bununla todo-ların sayını hesablayırıq
-        let list=document.querySelectorAll("li")
        
         if (firstTodo) {
             //Əgər output elementi içərisində əvvəlcədən əlavə edilmiş todo varsa yeni todo digərlərinin əvvəlinə əlavə edilir
@@ -36,9 +31,6 @@ function addTodo(){
         } else {
             output.appendChild(li);
         }
-
-          //Todo-larin sayını ekrana yazdırır
-          info.querySelector("p").innerHTML=+1 //Əgər ekranda 5 li elementi varsa 0 indeksi nəzərə aldığı üçün ekrana 4 todo sayı yazılacaq. Bunun üçün  lists.length+1  yazaraq bunu həll edirik
 
         //Todo əlavə edildikdən sonra input dəyəri sıfırlanması üçün
         input.value = "";
@@ -60,7 +52,7 @@ function searchTodo(){
 }
 function editTodo(element){
       // Klik olunan edit ikonunun ana elementi içərisindən  span içindəki input elementini seçir
-    let todo = element.parentElement.parentElement.querySelector("span input");
+    let todo = element.parentElement.querySelector("input");
     let oldValue = todo.value; //Inputun ilk deyerini saxlayır
     todo.removeAttribute("readonly", "readonly"); // Readonly atributunu silir və input redaktə edilə bilir
     todo.selectionStart = todo.value.length; //İnput carret-i input dəyərinin sonuna qoymaq üçün
@@ -80,10 +72,8 @@ function editTodo(element){
 
 //Todo silinməsi üçün funksiya
 function deleteTodo(e){
-    let list=document.querySelectorAll("li")
     let output=document.getElementById("output")
-    output.removeChild(e.parentElement.parentElement)
-    info.querySelector("p").innerHTML=list.length-1;
+    output.removeChild(e.parentElement)
 
     //Todo silindikde Localstoragede data deyeri yenilenir
     localStorage.setItem("data",output.innerHTML)
@@ -101,7 +91,6 @@ window.addEventListener("load",function(){
     let data=localStorage.getItem("data");
 output.innerHTML=data;
 let lists=output.querySelectorAll("li");
-info.querySelector("p").innerHTML=lists.length
 })
 
 /*LI elementlərinin sıralana bilməsi üçün funksiya SORTABLE kitabxanasi vasitəsi ilə. Kitabxana CDN link ilə HTML faylına qoşulub
